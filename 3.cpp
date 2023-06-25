fa52c4e390ec29cb53adced3d505b8d4d90e3c01
Huseyn <haciyevhuseyn2008@gmail.com>
2023-06-25 21:37:41 +0400

    changed 3 question

--- 3.cpp
+++ 3.cpp
@@ -1,18 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string& s) {
-        unordered_set<char> chars;
-        int maxSize = 0;
-        int i = 0, j = 0;
-        while (j < s.size()){
-            while (chars.find(s[j]) != chars.end()){
-                chars.erase(s[i]);
-                ++i;
+        int l = 0;
+        int answer = 0;
+        unordered_set<char> st;
+        for (int r = 0; r < s.size(); r++)
+        {
+            if (st.count(s[r])==0)
+            {
+                st.insert(s[r]);
+                answer = max(answer, r-l+1);
+            }else{
+                while (st.count(s[r]))
+                {
+                    st.erase(s[l]);
+                    l++;
+                }
+                st.insert(s[r]);
             }
-            maxSize = max(maxSize, j - i + 1);
-            chars.insert(s[j]);
-            ++j;
         }
-        return maxSize;
+        return answer;
     }
 };
